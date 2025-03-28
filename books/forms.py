f9574525c21ca7cd7d45288ced1b0d5c9d099ef7
TsertsvadzeE <eliso.tsertsvadze99@gmail.com>
2025-03-29 03:15:33 +0400

    part2

--- books/forms.py
+++ books/forms.py
@@ -9,6 +9,7 @@ class AuthorForm(forms.ModelForm):
 
 
 class BookForm(forms.ModelForm):
+    release_date = forms.DateField(widget=forms.DateInput(attrs={"type":"date"} ))
     class Meta:
         model = Book
         fields = ["title", "description", "author", "genre", "release_date", "personal_thoughts"]
--- books/views.py
+++ books/views.py
@@ -1,70 +1,63 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from . models import Book, Author
 from .forms import BookForm, AuthorForm
 
-def book_new(request):
-    if request.method == "POST":
-        form = BookForm(request.POST)
-        if form.is_valid():
-            book = form.save(commit=False)
-            book.save()
-            return redirect("book_detail", pk=book.pk)
-    else:
-        form = BookForm()
-    return render(request, "books/book_edit.html", {"form": form})
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/book_list.html'
+    context_object_name = 'books'
 
 
-def book_detail(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    return render(request, "Books/book_detail.html", {"book":book})
-
-def book_edit(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == "POST":
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid():
-            book = form.save(commit=False)
-            return redirect("book_detail", pk=book.pk)
-    else:
-        form = BookForm(instance=book)
-    return render(request, "books/book_edit.html", {"form": form})
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
 
 
-def book_list(request):
-    books =Book.objects.all()
-    return render(request, "books/book_list.html", {"books":books})
+class BookCreateView(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'books/book_edit.html'
+    success_url = reverse_lazy('book_list')
+
+class BookUpdateView(UpdateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'books/book_edit.html'
+    success_url = reverse_lazy('book_list')
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'books/book_confirm_delete.html'
+    success_url = reverse_lazy('book_list')
 
 
-def author_new(request):
-    if request.method == "POST":
-        form = AuthorForm(request.POST)
-        if form.is_valid():
-            author = form.save(commit=False)
-            author.save()
-            return redirect("author_detail", pk=author.pk)
-
-    else:
-        form = AuthorForm()
-    return render(request, "authors/author_edit.html", {"form": form})
+class AuthorListView(ListView):
+    model = Author
+    template_name = 'authors/author_list.html'
+    context_object_name = 'authors'
 
 
-def author_detail(request, pk):
-    author = get_object_or_404(Author, pk=pk)
-    return render(request, "authors/author_detail.html", {"author":author})
+class AuthorDetailView(DetailView):
+    model = Author
+    template_name = 'authors/author_detail.html'
 
 
-def author_edit(request, pk):
-    author = get_object_or_404(Author, pk=pk)
-    if request.method == "POST":
-        form = AuthorForm(request.POST, instance=author)
-        if form.is_valid():
-            author = form.save(commit=False)
-            return redirect("author_detail", pk=author.pk)
-    else:
-        form = AuthorForm(instance=author)
-    return render(request, "authors/author_edit.html", {"form": form})
+class AuthorCreateView(CreateView):
+    model = Author
+    form_class = AuthorForm
+    template_name = 'authors/author_edit.html'
+    success_url = reverse_lazy('author_list')
 
 
-def author_list(request):
-    authors = Author.objects.all()
-    return render(request, "authors/author_list.html", {"authors":authors})
+class AuthorUpdateView(UpdateView):
+    model = Author
+    form_class = AuthorForm
+    template_name = 'authors/author_edit.html'
+    success_url = reverse_lazy('author_list')
+
+
+class AuthorDeleteView(DeleteView):
+    model = Author
+    template_name = 'authors/author_confirm_delete.html'
+    success_url = reverse_lazy('author_list')
